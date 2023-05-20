cf3c856187be45efec6a9a2170dcba207a825b9b
adronmarsh <113702590+adronmarsh@users.noreply.github.com>
2023-05-20 15:33:37 +0200

    Se han parcheado bugs

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -8,24 +8,28 @@
             <h2>Chats</h2>
             <ul class="list-group">
                 @foreach ($chats as $chat)
-                    <a class="text-decoration-none" href="{{ route('chat.show', $chat->receiver->id) }}">
-                        <li class="list-group-item text-start">
-                            @auth
-                                @if (Auth::user()->id == $chat->user_id)
+                    @auth
+                        @if (Auth::user()->id == $chat->user_id)
+                            <a class="text-decoration-none" href="{{ route('chat.show', $chat->receiver->id) }}">
+                                <li class="list-group-item text-start">
                                     <img class="avatar rounded-circle img-ms"
                                         src="{{ $chat->receiver->avatar != null ? $chat->receiver->avatar : asset('media/default-avatar.png') }}"
                                         alt="Foto de perfil de {{ $chat->receiver->user }}">
 
                                     {{ $chat->receiver->user }}
-                                @else
+                                </li>
+                            </a>
+                        @else
+                            <a class="text-decoration-none" href="{{ route('chat.show', $chat->user->id) }}">
+                                <li class="list-group-item text-start">
                                     <img class="avatar rounded-circle img-ms"
                                         src="{{ $chat->user->avatar != null ? $chat->user->avatar : asset('media/default-avatar.png') }}"
                                         alt="Foto de perfil de {{ $chat->user->user }}">
                                     {{ $chat->user->user }}
-                                @endif
-                            @endauth
-                        </li>
-                    </a>
+                                </li>
+                            </a>
+                        @endif
+                    @endauth
                 @endforeach
             </ul>
         </div>
--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -3,44 +3,46 @@
 @section('title', 'Chat')
 
 @section('content')
-        <div class="m-auto">
-            @if ($chat)
-                <div class="chat-avatar-container">
-                    <img class="avatar rounded-circle img-ms"
-                        src="{{ $receiver->avatar != null ? $receiver->avatar : asset('media/default-avatar.png') }}"
-                        alt="Foto de perfil de {{ $receiver->user }}">
+    <div class="m-auto">
+        @if ($chat)
+            <div class="chat-avatar-container">
+                <img class="avatar rounded-circle img-ms"
+                    src="{{ $receiver->avatar != null ? $receiver->avatar : asset('media/default-avatar.png') }}"
+                    alt="Foto de perfil de {{ $receiver->user }}">
+                <a href="/users/{{ $receiver->id }}" class="text-decoration-none text-reset">
                     <h1>{{ $receiver->user }}</h1>
-                </div>
+                </a>
+            </div>
 
-                <div class="chat-container">
-                    <div class="messages">
-                        @foreach ($messages as $message)
-                            <div class="message @if ($message->user_id === Auth::user()->id) text-end @else text-start @endif">
-                                <div
-                                    class="m-1 message-content d-inline-block d-inline-block rounded @if ($message->user_id === $receiver->id) bg-secondary text-white @else bg-primary text-white @endif">
-                                    <p class="p-2 m-0">{{ $message->content }}</p>
-                                </div>
+            <div class="chat-container">
+                <div class="messages">
+                    @foreach ($messages as $message)
+                        <div class="message @if ($message->user_id === Auth::user()->id) text-end @else text-start @endif">
+                            <div
+                                class="m-1 message-content d-inline-block d-inline-block rounded @if ($message->user_id === $receiver->id) bg-secondary text-white @else bg-primary text-white @endif">
+                                <p class="p-2 m-0">{{ $message->content }}</p>
                             </div>
-                        @endforeach
-                    </div>
-                    <div class="input-chat-bottom bg-light position-fixed ">
-                        <form method="POST" action="{{ route('messages.store', $chat->id) }}" id="message-form">
-                            @csrf
-
-                            <div class="form-group d-flex justify-content-end">
-                                <textarea class="form-control" id="content" name="content" rows="3"></textarea>
-                                <button type="submit" class="btn btn-link">
-                                    <i class="bi bi-arrow-right-circle-fill bi-lg"></i>
-                                </button>
-                            </div>
-
-                            <input type="hidden" name="chat_id" value="{{ $chat->id }}">
-                            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                        </form>
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
-            @else
-                <p>No se encontró ningún chat.</p>
-            @endif
-        </div>
+                <div class="input-chat-bottom bg-light position-fixed ">
+                    <form method="POST" action="{{ route('messages.store', $chat->id) }}" id="message-form">
+                        @csrf
+
+                        <div class="form-group d-flex justify-content-end">
+                            <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                            <button type="submit" class="btn btn-link">
+                                <i class="bi bi-arrow-right-circle-fill bi-lg"></i>
+                            </button>
+                        </div>
+
+                        <input type="hidden" name="chat_id" value="{{ $chat->id }}">
+                        <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                    </form>
+                </div>
+            </div>
+        @else
+            <p>No se encontró ningún chat.</p>
+        @endif
+    </div>
 @endsection

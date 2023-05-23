d58dc3fafed6c893768dba86b2c24bf3009508f4
adronmarsh <113702590+adronmarsh@users.noreply.github.com>
2023-05-23 12:06:49 +0200

    Se han añadido sql para usar por defecto

---
+++ database/example/posts.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts (user_id, title, content, url, created_at, updated_at) VALUES
+(1, 'Título del primer post', 'Contenido del primer post', 'https://ejemplo.com/post1', NOW(), NOW());
+(2, 'Título del segundo post', 'Contenido del segundo post', 'https://ejemplo.com/post2', NOW(), NOW());
+(1, 'Título del tercer post', 'Contenido del tercer post', 'https://ejemplo.com/post3', NOW(), NOW());
+
---
+++ database/example/user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (rol, user, email, password) VALUES
+ ('particular', 'John Doe', 'johndoe@example.com', 'password123'),
+ ('particular', 'Jane Smith', 'janesmith@example.com', 'password123'),
+ ('corporation', 'Calzados Martínez', 'calzados@example.com', 'password123'),
+ ('corporation', 'Fresh Fresa', 'fresh@example.com', 'password123'),
+ ('corporation', 'Peluquería Valencia', 'peluqueria@example.com', 'password123');
